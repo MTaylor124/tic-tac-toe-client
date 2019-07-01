f7bafb6e466e577d416e9d776889c0fc3e522352
MTaylor124 <matthew_taylor1@student.uml.edu>
2019-07-01 12:44:55 -0400

    Changes to urls

--- curl-scripts/auth/games.sh
+++ curl-scripts/auth/games.sh
@@ -1,6 +1,6 @@
 # VARIABLE=VALUE sh curl-scripts/auth/games.sh
 
-curl "https://tic-tac-toe-wdi.herokuapp.com/games/${ID}" \
+curl "https://tic-tac-toe-wdi.herokuapp.com/games" \
   --include \
   --request POST \
   --header "Content-Type: application/json" \
